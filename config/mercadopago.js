eedc59016f79cbaf2d3cbfb66c889f01c5c3471a
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-03-20 20:17:54 -0300

    Alterando variaveis de ambiente

--- config/mercadopago.js
+++ config/mercadopago.js
@@ -1,10 +1,11 @@
 // SDK de Mercado Pago
+require('dotenv').config();
 const mercadopago = require('mercadopago');
 
 // Configura credenciais
 mercadopago.configure({
-    access_token: 'APP_USR-334491433003961-030821-12d7475807d694b645722c1946d5ce5a-725736327',
-    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+    access_token: process.env.access_token,
+    integrator_id: process.env.integrator_id,
 });
 
 module.exports = mercadopago;
