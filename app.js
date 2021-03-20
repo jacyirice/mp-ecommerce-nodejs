544cf376b7929ec30ad76abeeb302ed1240740ab
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-03-20 20:05:29 -0300

    Adicionando checkout

--- app.js
+++ app.js
@@ -1,22 +1,89 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000
-
+var mercadopago = require('./config/mercadopago')
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
+app.get('/detail', function(req, res) {
     res.render('detail', req.query);
+});
+app.post('/notification', function(req, res) {
+    console.log(req.body)
+    res.status(200).json({})
+})
+app.get('/checkout', function(req, res) {
+    res.render('checkout', req.query);
+});
+
+app.get('/payment/fail', function(req, res) {
+    res.render('payment-status/fail', req.query);
+});
+app.get('/payment/in-process', function(req, res) {
+    res.render('payment-status/in_process', req.query);
+});
+app.get('/payment/success', function(req, res) {
+    res.render('payment-status/success', req.query);
+});
+
+app.post("/create_preference", (req, res) => {
+    let preference = {
+        items: [{
+            title: req.body.title,
+            quantity: Number(req.body.quantity),
+            description: req.body.description,
+            picture_url: req.body.img,
+            currency_id: 'BRL',
+            unit_price: Number(req.body.price),
+        }],
+        external_reference: "jacyirice@gmail.com",
+        payer: {
+            name: req.body.payer.name,
+            surname: req.body.payer.surname,
+            email: req.body.payer.email,
+            phone: {
+                area_code: req.body.payer.phone.area_code,
+                number: Number(req.body.payer.phone.number)
+            },
+
+            address: {
+                street_name: req.body.payer.address.street_name,
+                street_number: Number(req.body.payer.address.street_number),
+                zip_code: req.body.payer.address.zip_code
+            }
+        },
+        back_urls: {
+            "success": "http://localhost:3000/payment/success",
+            "failure": "http://localhost:3000/payment/fail",
+            "pending": "http://localhost:3000/payment/in-process"
+        },
+        auto_return: 'approved',
+        payment_methods: {
+            excluded_payment_methods: [{
+                id: "amex"
+            }],
+            installments: 6
+        }
+        // notification_url: "https://www.your-site.com/ipn",
+    };
+    mercadopago.preferences.create(preference)
+        .then(function(response) {
+            res.json({ id: response.body.id })
+        }).catch(function(error) {
+            console.log(error);
+        });
 });
 
 app.listen(port);
---
+++ assets/checkout.js
@@ -0,0 +1,65 @@
+//Handle call to backend and generate preference.
+document.getElementById("form-checkout").addEventListener("submit", function(e) {
+    $('#checkout-btn').attr("disabled", true);
+    var orderData = {
+        quantity: document.getElementById("product-unit").value,
+        title: document.getElementById("product-title").value,
+        description: document.getElementById("product-description").value,
+        price: document.getElementById("product-price").value,
+        img: document.getElementById("product-img").value,
+        payer: {
+            name: document.getElementById("payer-name").value,
+            surname: document.getElementById("payer-surname").value,
+            email: document.getElementById("payer-email").value,
+            phone: {
+                area_code: document.getElementById("payer-phone-area_code").value,
+                number: document.getElementById("payer-phone-number").value,
+            },
+            address: {
+                street_name: document.getElementById("payer-address-street_name").value,
+                street_number: document.getElementById("payer-address-street_number").value,
+                zip_code: document.getElementById("payer-address-zip_code").value,
+            }
+        },
+    };
+    console.log($('#product-unit').val(), );
+    console.log(document.getElementById("product-unit"));
+    fetch("/create_preference", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(orderData),
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(preference) {
+            console.log(preference)
+            console.log(preference.id)
+            createCheckoutButton(preference.id);
+            $(".shopping-cart").fadeOut(500);
+
+        })
+        .catch(function(error) {
+            console.log('oi');
+            console.log(error);
+            alert("Unexpected error1");
+            $('#checkout-btn').attr("disabled", false);
+        });
+    e.preventDefault();
+});
+
+//Create preference when click on checkout button
+function createCheckoutButton(preference) {
+    var script = document.createElement("script");
+
+    // The source domain must be completed according to the site for which you are integrating.
+    // For example: for Argentina ".com.ar" or for Brazil ".com.br".
+    script.src = "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js";
+    script.setAttribute("data-button-label", "Pague a compra")
+    script.type = "text/javascript";
+    script.dataset.preferenceId = preference;
+    document.getElementById("button-checkout").innerHTML = '';
+    document.querySelector("#button-checkout").appendChild(script);
+}
